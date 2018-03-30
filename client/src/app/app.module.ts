8df6b3dbffe0e0f5d32e58f58be3c138f0294922
François David <francois.david@ibm.com>
2018-03-30 17:02:23 +0200

    Fixed somme front issues

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,22 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
 import { ChatComponent } from './components/chat/chat.component';
 
+import { MessagesService } from './services/messages.service';
+import { HomeComponent } from './components/home/home.component';
+// import { MessageComponent } from './components/message/message.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'chat', component: ChatComponent },
+  // No PageNotFound Handling Yet
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    ChatComponent
+    ChatComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [
+    MessagesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
